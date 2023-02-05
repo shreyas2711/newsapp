f7d9f3b9458cc6703696e4a637ac6f757646bb65
Shreyas <shreyas.kamath27@gmail.com>
2023-02-05 15:23:41 +0530

    Commit added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar';
-
 import React, { Component } from 'react'
 import News from './components/News';
+import LoadingBar from 'react-top-loading-bar'
 
 import {
   BrowserRouter as Router,
@@ -11,30 +11,45 @@ import {
 } from "react-router-dom";
 
 export default class App extends Component {
-  pageSize=7
+  pageSize=5
+  apiKey=process.env.REACT_APP_NEWS_API
+  
+  state={
+    progress:0
+  }
+
+  setProgress=(progress)=>{
+    this.setState({progress:progress})
+  }
+
   render() {
     return (
       <div>
-      <Router>  
+      <Router> 
      <Navbar/>
+     <LoadingBar height={3}
+        color='#f11946'
+        progress={this.state.progress}
+      />
      <Routes>
-          {/* <Route exact path="/"><News key="general" pageSize={5} country="in" category="general"/></Route>
-          <Route exact path="/business"><News key="business" pageSize={5} country="in" category="business"/></Route>
-          <Route exact path="/entertainment"><News key="entertainment" pageSize={5} country="in" category="entertainment"/></Route>
-          <Route exact path="/general"><News key="general" pageSize={5} country="in" category="general"/></Route>
-          <Route exact path="/health"><News key="health" pageSize={5} country="in" category="health"/></Route>
-          <Route exact path="/science"><News key="science" pageSize={5} country="in" category="science"/></Route>
-          <Route exact path="/sports"><News key="sports" pageSize={5} country="in" category="sports"/></Route>
-          <Route exact path="/technology"><News key="technology" pageSize={5} country="in" category="technology"/></Route>
+     
+          {/* <Route exact path="/"><News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={5} country="in" category="general"/></Route>
+          <Route exact path="/business"><News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={5} country="in" category="business"/></Route>
+          <Route exact path="/entertainment"><News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={5} country="in" category="entertainment"/></Route>
+          <Route exact path="/general"><News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={5} country="in" category="general"/></Route>
+          <Route exact path="/health"><News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={5} country="in" category="health"/></Route>
+          <Route exact path="/science"><News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={5} country="in" category="science"/></Route>
+          <Route exact path="/sports"><News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={5} country="in" category="sports"/></Route>
+          <Route exact path="/technology"><News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={5} country="in" category="technology"/></Route>
      <Routes> */}
-     <Route exact path="/" element = {<News key="general" pageSize = {this.pageSize} country = "in" category = "general"/>}/>
-     <Route exact path="/business" element= {<News key="business" pageSize = {this.pageSize} country="in" category="business"/>}/>
-            <Route exact path="/entertainment" element= {<News key="entertainment" pageSize = {this.pageSize} country="in" category="entertainment"/>}/>
-            <Route exact path="/general" element= {<News key="general" pageSize = {this.pageSize} country="in" category="general"/>}/>
-            <Route exact path="/health" element= {<News key="health" pageSize = {this.pageSize} country="in" category="health"/>}/>
-            <Route exact path="/science" element= {<News key="science" pageSize = {this.pageSize} country="in" category="science"/>}/>
-            <Route exact path="/sports" element= {<News key="sports" pageSize = {this.pageSize} country="in" category="sports"/>}/>
-            <Route exact path="/technology" element= {<News key="technology" pageSize = {this.pageSize} country="in" category="technology"/>}/>
+     <Route exact path="/" element = {<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize = {this.pageSize} country = "in" category = "general"/>}/>
+     <Route exact path="/business" element= {<News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize = {this.pageSize} country="in" category="business"/>}/>
+            <Route exact path="/entertainment" element= {<News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize = {this.pageSize} country="in" category="entertainment"/>}/>
+            <Route exact path="/general" element= {<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize = {this.pageSize} country="in" category="general"/>}/>
+            <Route exact path="/health" element= {<News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize = {this.pageSize} country="in" category="health"/>}/>
+            <Route exact path="/science" element= {<News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize = {this.pageSize} country="in" category="science"/>}/>
+            <Route exact path="/sports" element= {<News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize = {this.pageSize} country="in" category="sports"/>}/>
+            <Route exact path="/technology" element= {<News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize = {this.pageSize} country="in" category="technology"/>}/>
             
           </Routes>
         </Router>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title,description,imageUrl,newsUrl,author,date,source}=this.props
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props
     return (
       <div className="my-3">
         <div className="card">
-        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:"92%",zIndex:'1'}}>{source}</span>
-            <img src={!imageUrl?"https://images.hindustantimes.com/img/2023/02/03/1600x900/nirmala_sitharaman_1675419085172_1675419092193_1675419092193.JPG":imageUrl}className="card-img-top" alt="..."/>
-            <div className="card-body">
+          <div style={{display:'flex',position:'absolute',justifyContent:'flex-start',left:'0'}}>
+
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+          <img src={!imageUrl ? "https://images.hindustantimes.com/img/2023/02/03/1600x900/nirmala_sitharaman_1675419085172_1675419092193_1675419092193.JPG" : imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <p className="card-text"><small className='text-muted'>By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+            <p className="card-text"><small className='text-muted'>By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
             <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
-            </div>
-            </div>
-            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
